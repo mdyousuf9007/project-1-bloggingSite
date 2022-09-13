3f100d4081e3fe8ed076e09418b25d547a439381
mdyousuf <yousuf9007@gmail.com>
2022-09-13 13:01:08 +0530

    final project done

--- src/models/blogsModel.js
+++ src/models/blogsModel.js
@@ -1,47 +1,51 @@
-const mongoose=require("mongoose")
-const ObjectId=mongoose.Schema.Types.ObjectId
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const blogsSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
+const blogsSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    body:{
-       type:String,
-       required:true
+    body: {
+      type: String,
+      required: true,
     },
-    authorId:{
-        type:ObjectId,
-        ref:"Author",
-        required:true
+    authorId: {
+      type: ObjectId,
+      ref: "Author",
+      required: true,
     },
-    tags:{type:[String]
-    },
-    category:[{
-        type:String,
-        required:true
-    }],
-    subcategory:[{
-        type:String,
-        required:true
-    }],
+    tags: { type: [String] },
+    category: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    subcategory: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
     isDeleted: {
-         type: Boolean,
-          default: false
-        },
-    isPublished: {
-         type: Boolean,
-          default: false
-        },
-    deletedAt:{
-        type:Date,
-        default:null
+      type: Boolean,
+      default: false,
     },
-    publishedAt:{
-        type:Date,
-        default:null
-    }
- 
-},{timestamps:true})
-module.exports=mongoose.model("Blog",blogsSchema)
-
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+    publishedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Blog", blogsSchema);
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,40 @@
-const express=require("express")
- const router=express.Router()
+const express = require("express");
+const router = express.Router();
 
-const{ authorCreate,loginAuthor}=require("../controllers/authorController")
-const {createBlogs, getBlogs,updateBlog, deleteBlog,deleteByQuery}=require("../controllers/blogsController")
-const {auth}=require("../middlewares/auth")
+const {
+  authorCreate,
+  loginAuthor,
+} = require("../controllers/authorController");
+const {
+  createBlogs,
+  getBlogs,
+  updateBlog,
+  deleteBlog,
+  deleteByQuery,
+} = require("../controllers/blogsController");
+const { auth } = require("../middlewares/auth");
 //.......................my all Apis....
 //============post api create author========
-router.post("/authors",authorCreate)
-//============post api clogin author========
-router.post("/login",loginAuthor)
+router.post("/authors", authorCreate);
+//============post api login author========
+router.post("/login", loginAuthor);
 //============post api create blogs=========
-router.post("/blogs",auth,createBlogs)
+router.post("/blogs", auth, createBlogs);
 //============get api get blogs=============
-router.get("/blogs",auth,getBlogs)
+router.get("/blogs", auth, getBlogs);
 //============put-api=======================
-router.put("/blogs/:blogId",auth,updateBlog)
+router.put("/blogs/:blogId", auth, updateBlog);
 //===========delete api by path params======
-router.delete("/blogs/:blogId",auth,deleteBlog)
+router.delete("/blogs/:blogId", auth, deleteBlog);
 //===========delete api by query params======
-router.delete("/blogs",auth,deleteByQuery)
+router.delete("/blogs", auth, deleteByQuery);
 
 //=============in the put api and delete api if the blogid is not present============
 //=============in the params so this api return the erorr in the response============
-router.all("/*", function(req,res){
-    res.status(404).send({status:false,msg:"blogId is not present in the path params"})
-})
+router.all("/*", function (req, res) {
+  res
+    .status(404)
+    .send({ status: false, msg: "please provide BlogId in path param" });
+});
 
-
-
-module.exports=router
-
-
-
-
-
-
-
-
-
-
+module.exports = router;
